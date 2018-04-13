77426e967ea60bdd3a1704a64dc8ef21a1dc4d12
Roman <lytvynenko.r.i@gmail.com>
2018-04-13 22:41:50 +0300

    add method size

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,17 +1,17 @@
+import java.util.Scanner;
+
 public class BubbleSort {
     public static void main(String[] args) {
         print(sort(fill(array)));
     }
-    static private int size = 15;
-    static private int[] array = new int[size];
-
+//    static private int size = 15;
+    static private int[] array = new int[size()];
 
     static void print(int[] array){
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
     }
-
     static int[] fill(int[] array){
         for (int i = 0; i < array.length; i++) {
             array[i] = (int)(Math.round(Math.random()*100));
@@ -31,4 +31,10 @@ public class BubbleSort {
         return array;
     }
 
+    static int size(){
+        System.out.print("Enter size of array: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
 }
